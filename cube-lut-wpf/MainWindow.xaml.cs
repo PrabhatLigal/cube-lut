29fc1454999534d564306be91eabc24523a9ec3a
Prabhat <ligal3245@gmail.com>
2017-04-17 23:50:53 +0545

    cube generator and basic wpf app

---
+++ cube-lut-wpf/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using cube_lut;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cube_lut_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateImage_Click(object sender, RoutedEventArgs e)
+        {
+
+            var image = Hald.GenerateClutImage(8);
+
+
+            IntPtr len;
+            SKImage img = SKImage.FromBitmap(image);
+
+
+            SKData data = img.Encode(SKImageEncodeFormat.Png, 90);
+            using (FileStream fs = new FileStream("test.png", FileMode.Create))
+            {
+                data.SaveTo(fs);
+            }
+        }
+
+        private void GenerateCube_Click(object sender, RoutedEventArgs e)
+        {
+            //  string mydocpath =
+            //  Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var lines = new List<string>();
+            var filename = "test.cube";
+            if (Hald.ConvertClutImageToCube("test.jpg", out lines))
+            {
+                //using (StreamWriter outputFile = new StreamWriter("test.cube"))
+                //{
+                //    foreach (string line in lines)
+                //        outputFile.WriteLine(line);
+                //}
+
+                if (File.Exists(filename))
+                {
+                    File.Delete(filename);
+                }
+                File.WriteAllLines(filename, lines.ToArray());
+
+            }
+
+
+        }
+    }
+}
--- cube-lut/hald.cs
+++ cube-lut/hald.cs
@@ -10,36 +10,79 @@ namespace cube_lut
 {
     public class Hald
     {
-        SKBitmap GenerateClutImage(int level)
+        static public  SKBitmap GenerateClutImage(int level)
         {
             var cube_size = level * level;
             var image_size = level * level * level;
             //data = p = malloc((sizeof *data) * image_size * image_size * 3);
             var image = new SKBitmap(image_size, image_size);
 
-
+            int i = 0;
             for (var blue = 0; blue < cube_size; blue++)
             {
                 for (var green = 0; green < cube_size; green++)
                 {
                     for (var red = 0; red < cube_size; red++)
                     {
-                        var R = (byte)((float)red / (float)(cube_size - 1) * 255);
-                        var G = (byte)((float)green / (float)(cube_size - 1) * 255);
-                        var B = (byte)((float)blue / (float)(cube_size - 1) * 255);
+                        var R = (byte)(((float)red   / (float)(cube_size - 1)) * 255);
+                        var G = (byte)(((float)green / (float)(cube_size - 1)) * 255);
+                        var B = (byte)(((float)blue  / (float)(cube_size - 1)) * 255);
 
-                        var pos = red + green + blue;
-
-                        var x = pos % image_size;
-                        var y = pos / image_size;
+                       // var pos = red + green + blue;
+                
+                        var x = i % image_size;
+                        var y = i / image_size;
                         var color = new SKColor(R, G, B);
                         image.SetPixel(x, y, color);
-
+                        i++;
                     }
                 }
             }
 
             return image;
+        }
+
+        static public bool ConvertClutImageToCube(string imagepath, out List<string> Cube)
+        {
+            var bitmap = SKBitmap.Decode(imagepath);
+            Cube = new List<string>();
+            List<string> data = new List<string>();
+            if (bitmap.Width != bitmap.Height) return false;
+
+            var lutSize = Math.Round( Math.Pow(bitmap.Width, 1 / 3.0));
+            var count = (int)(Math.Pow(lutSize, 6));
+            if (bitmap.Pixels.Length != count) return false;
+
+
+            data.Add( string.Format("LUT_3D_SIZE  {0}", lutSize*lutSize));
+            data.Add("DOMAIN_MIN 0.0 0.0 0.0");
+            data.Add("DOMAIN_MAX 1.0 1.0 1.0");
+
+            // {r, (3 * g + b)/4.0, b}
+            //int j = 0;
+            //for (var blue = 0; blue < lutSize; blue++)
+            //{
+            //    for (var green = 0; green < lutSize; green++)
+            //    {
+            //        for (var red = 0; red < lutSize; red++)
+            //        {
+            //            var i = bitmap.Pixels[j];
+            //            data.Add(string.Format("{0} {1} {2}", i.Red / 255.0, i.Green / 255.0, i.Blue / 255.0));
+            //            //data.Add(string.Format("{0} {1} {2}", i.Red / 255.0, (3*i.Green +i.Blue) / (4.0* 255.0), i.Blue / 255.0));
+            //        }
+            //    }
+            //}
+            foreach(var i in bitmap.Pixels)
+            {
+                data.Add(string.Format("{0} {1} {2}", i.Red / 255.0, i.Green / 255.0, i.Blue / 255.0));
+            }
+            Cube = data;
+
+
+
+
+
+            return true;
         }
     }
 
